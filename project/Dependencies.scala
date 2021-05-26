15e8d711f85716dc7cb95f1980aa590223ed582c
Ryan Latture <rlatture@citrine.io>
2021-05-26 08:01:01 -0700

    bumped log4j version from 2.14.1 to 2.11.0.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,5 @@ import sbt._
 object Dependencies {
   lazy val breeze = "org.scalanlp"             %% "breeze"           % "1.1"
   lazy val jblas  = "org.jblas"                %  "jblas"            % "1.2.5"
-  lazy val log4j  = "org.apache.logging.log4j" %  "log4j-slf4j-impl" % "2.14.1"
+  lazy val log4j  = "org.apache.logging.log4j" %  "log4j-slf4j-impl" % "2.11.0"
 }
